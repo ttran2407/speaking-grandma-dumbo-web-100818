12e68cc6352cb78fb455a39ed98068f907187cb6
Tien Tran <tientran2407.vn@gmail.com>
2018-09-26 21:23:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,20 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma (string)
+
+string1 = string
+stringup = string.upcase
+
+if string == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+  
+elsif string1 == stringup
+  puts "NO, NOT SINCE 1938!"
+
+else puts "HUH?! SPEAK UP, SONNY!"
+  
+end
+
+end
